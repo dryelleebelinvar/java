35e5e65b5d639ef958b01aea933b9f9d423d40d3
unknown <dryelle.silva@varsolutions.com>
2023-01-27 16:52:16 -0300

    update

---
+++ MeuPrimeiroCodigoJava/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;  //caminho do arquivo
+
+public class Main {  //classe
+    public static void main(String[] args)  //método(ação)
+    {
+        System.out.println("Olá Mundo!! ");   //comando do sistema //saída do sistema //imprimir saída do sistema
+        System.out.println("ESTE É O MEU PRIMEIRO CÓDIGO RM JAVA!!!");
+    }
+}
