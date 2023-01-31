d24b853421698225035ddfee09ef8306aff48ec7
unknown <dryelle.silva@varsolutions.com>
2023-01-31 15:53:53 -0300

    update

--- MeuPrimeiroCodigoJava/src/com/company/Main.java
+++ MeuPrimeiroCodigoJava/src/com/company/Main.java
@@ -31,15 +31,22 @@ public class Main {  //classe
         f = e = d = 50;
         System.out.println(f + e + d);
 
+
+
+
+
         String retornoMetodo = buscarSobrenome();
         System.out.println(retornoMetodo);
 
         String retornoMetodo2 = inserirNome();
         System.out.println(retornoMetodo2);
+
+        int idade = calcularIdadePorAno();
+        double imc = calcularIMC();
     }
 
     //tipo de acesso: (public ou private) (tipo de retorno) (nome do método) (parâmetros)
-    public static String buscarSobrenome(String nome)  //parâmetro
+    public static String buscarSobrenome()  //parâmetro
     {
         String sobrenome = "Ebelin";
         return sobrenome;
@@ -48,15 +55,75 @@ public class Main {  //classe
     public static String inserirNome()
     {
         Scanner ler = new Scanner(System.in);  //variável de leitura, interação com o usuário
+
         System.out.printf("Digite seu nome: ");
         String nome = ler.next();
         System.out.printf("Digite seu sobrenome: ");
         String sobrenome = ler.next();
 
-        System.out.println(nome + " " + sobrenome);
-
         String nomeCompleto = nome + " " + sobrenome;
-
         return nomeCompleto;
+    }
+
+    public static int calcularIdadePorAno()
+    {
+        Scanner ler = new Scanner(System.in);
+
+        System.out.printf("Digite seu ano de nascimento: ");
+        int dataNascimento = ler.nextInt();
+        int calculoNascimento = 2023 - dataNascimento;
+
+        String classificacao = " ";
+        if(calculoNascimento < 18)
+        {
+            classificacao = "menor de idade";
+        }
+        else
+        {
+            classificacao = "maior de idade";
+        }
+
+        System.out.println("Você tem " + calculoNascimento + " anos e é " + classificacao);
+
+        return calculoNascimento;
+    }
+
+    public static double calcularIMC()
+    {
+        Scanner ler = new Scanner(System.in);
+
+        System.out.printf("Digite sua altura: ");
+        double altura = ler.nextDouble();
+        System.out.printf("Digite seu peso: ");
+        double peso = ler.nextDouble();
+
+        double calculoIMC = peso / (altura * altura);
+        System.out.println("Seu imc é " + calculoIMC);
+
+        if(calculoIMC < 18.5) {
+            System.out.println("Você está abaixo do peso ideal!");
+        }
+        else if(calculoIMC >= 18.6 && calculoIMC <= 24.9)
+        {
+            System.out.println("Peso normal");
+        }
+        else if(calculoIMC >= 25 && calculoIMC <= 29.9)
+        {
+            System.out.println("Pré-obesidade");
+        }
+        else if(calculoIMC >= 30 && calculoIMC <= 34.9)
+        {
+            System.out.println("Obesidade grau I");
+        }
+        else if(calculoIMC >= 35 && calculoIMC <= 39.9)
+        {
+            System.out.println("Obesidade grau II");
+        }
+        else
+        {
+            System.out.println("Obesidade grau III");
+        }
+
+        return calculoIMC;
     }
 }
