36c6b82e6317698c69316fb30b5b3239f40669f7
unknown <dryelle.silva@varsolutions.com>
2023-02-02 15:29:09 -0300

    update

--- varsolutions/src/main/java/com/br/varsolutions/Pessoa.java
+++ varsolutions/src/main/java/com/br/varsolutions/Pessoa.java
@@ -5,8 +5,10 @@ public class Pessoa {
     String sobrenome;
     String endereco;
     int idade;
-    double peso;
-    double altura;
+    float peso;
+    float altura;
+    double salario;
+    String time;
 
     public String getNome(){return nome;}
     public void setNome(String nome){this.nome = nome;}
@@ -20,9 +22,17 @@ public class Pessoa {
     public int getIdade(){return idade;}
     public void setIdade(int idade){this.idade = idade;}
 
-    public double getPeso(){return peso;}
-    public void setPeso(double peso){this.peso = peso;}
+    public float getPeso(){return peso;}
+    public void setPeso(float peso){this.peso = peso;}
 
-    public double getAltura(){return altura;}
-    public void setAltura(double altura){this.altura = altura;}
+    public float getAltura(){return altura;}
+    public void setAltura(float altura){this.altura = altura;}
+
+    public double getSalario(){return salario;}
+
+    public void setSalario(double salario){this.salario = salario;}
+
+    public String getTime(){return time;}
+
+    public void setTime(String time){this.time = time;}
 }
--- varsolutions/src/main/java/com/br/varsolutions/PessoaController.java
+++ varsolutions/src/main/java/com/br/varsolutions/PessoaController.java
@@ -1,16 +1,17 @@
 package com.br.varsolutions;
 //configurações pré-definidas
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/pessoa")
+@CrossOrigin(origins = "*")
+@Slf4j  //log
 public class PessoaController {
     //endpoint
-    @CrossOrigin(origins = "*")
     @GetMapping
     public ResponseEntity<Object> get(){
         Pessoa pessoa1 = new Pessoa();
@@ -19,8 +20,66 @@ public class PessoaController {
         pessoa1.setEndereco("São Paulo, Brasil");
         pessoa1.setIdade(18);
         pessoa1.setPeso(53);
-        pessoa1.setAltura(1.66);
+        pessoa1.setAltura(1.66f);
 
         return ResponseEntity.ok(pessoa1);
+    }
+    @GetMapping("/resumo")
+    public ResponseEntity<Object> getPessoa(@RequestBody Pessoa pessoa){
+        String imc = null;
+        int anoNascimento = 0;
+        String impostoRenda = null;
+        String validaMundial = null;
+
+        if(!pessoa.getNome().isEmpty()) {
+
+            log.info("Iniciando o processo de resumo da pessoa: ", pessoa);
+
+            if(Objects.nonNull(pessoa.getPeso()) && Objects.nonNull(pessoa.getAltura())) {
+                log.info("Iniciando o calculo do IMC");
+                imc = calculaIMC(pessoa.getPeso(), pessoa.getAltura());
+            }
+
+            if(Objects.nonNull(pessoa.getIdade())){
+                log.info("Iniciando o calculo do ano de nascimento");
+                anoNascimento = calculaAnoNascimento(pessoa.getIdade());
+            }
+
+            if(Objects.nonNull(pessoa.getSalario())){
+                log.info("Iniciando do calculo do imposto de reda");
+                impostoRenda = calculaFaixaImpostoRenda(pessoa.getSalario());
+            }
+
+            if(Objects.nonNull(pessoa.getTime())){
+                log.info("Validando se o time de coração tem Mundial");
+                validaMundial = calculaMundial(pessoa.getTime());
+            }
+
+            log.info("Montando objeto de retorno para o front-end.");
+            Object resumo = montarRespostaFrontEnd(imc, anoNascimento, impostoRenda, validaMundial);
+
+            return ResponseEntity.ok(resumo);
+        }
+        return ResponseEntity.noContent().build();
+    }
+
+    private Object montarRespostaFrontEnd(String imc, int anoNascimento, String impostoRenda, String validaMundial) {
+
+    }
+
+    private String calculaMundial(String time) {
+
+    }
+
+    private String calculaFaixaImpostoRenda(double salario) {
+
+    }
+
+    private int calculaAnoNascimento(int idade) {
+
+    }
+
+    private String calculaIMC(float peso, float altura) {
+
     }
 }
